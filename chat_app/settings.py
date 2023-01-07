0ebba4684c86fa072c5cec610a026fe5669949c6
unknown <P.zelenak1@gmail.com>
2023-01-07 13:35:38 +0100

    all

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+import dj_database_url
+import os  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-(v*yi1&3=bfimbqky65^kb-+1$%%_gk_4o+li1bk21_l8&x@c3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -142,3 +145,6 @@ CHANNEL_LAYERS = {
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
